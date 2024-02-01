160f4316e0bfe7bd6982d77a2eb6fffef33901a4
Rodric <72948934+RodricBr@users.noreply.github.com>
2024-01-31 23:59:46 -0300

    Added colors to the strings

--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -10,11 +10,11 @@ const MSG_SIZE: usize = 32;
 
 fn main() {
   // Criando uma variável "cliente" mutável que é uma stream TCP e conectar ao IPv4 "LOCAL" (10.0.0.148:7777)
-  let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
-  client.set_nonblocking(true).expect("Failed to initiate non-blocking"); // Mesma coisa do server
-  
+  let mut client = TcpStream::connect(LOCAL).expect("\x1b[31mStream failed to connect\x1b[0m");
+  client.set_nonblocking(true).expect("\x1b[31mFailed to initiate non-blocking\x1b[0m"); // Mesma coisa do server
+
   let (tx, rx) = mpsc::channel::<String>(); // Mesma coisa do server
-  
+
   // Isso aqui tudo é a mesma coisa do server, lê lá!
   thread::spawn(move || loop{
     let mut buff = vec![0; MSG_SIZE];
@@ -23,11 +23,11 @@ fn main() {
         // Faz uma iteração e checa se as mensagens dentro do buffer são iguais a zero
         // se forem a gente colecta dentro de um vetor e discarta elas (undercore = variável inexistente)
         let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-        println!("Message received {:?}", msg);
+        println!("\x1b[32m> Message received in bytes\x1b[0m {:?}\n", msg);
       },
       Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
       Err(_) => {
-        println!("Connection with server was severed");
+        println!("\x1b[31m< Connection with server was severed\x1b[0m");
         break;
       }
     }
@@ -39,8 +39,8 @@ fn main() {
       Ok(msg) => {
         let mut buff = msg.clone().into_bytes(); // Variável buffer
         buff.resize(MSG_SIZE, 0); // Mudando o tamanho do buffer de acordo com o MSG_SIZE
-        client.write_all(&buff).expect("Writing to socket failed"); // Escrevendo todos os buffers no client
-        println!("Message sent {:?}", msg); // Se não houver exceções printa a mensagem
+        client.write_all(&buff).expect("\x1b[31mWriting to socket failed\x1b[0m"); // Escrevendo todos os buffers no client
+        println!("\x1b[32m> Message sent\x1b[0m {:?}", msg); // Se não houver exceções printa a mensagem
       },
       // Checando se o TryRecieveError está vazio, e se tiver joga pra um unit type "()"
       // O Unit Type é usado quando não há outro valor significativo que possa ser retornado.
@@ -53,7 +53,7 @@ fn main() {
     thread::sleep(Duration::from_millis(100));
   });
   // Esse print será criado quando o usuário abrir o client
-  println!("Write a Message:");
+  println!("\x1b[32mWrite a message in the buffer:\x1b[0m");
     loop {
       // Criando uma string mutável
       let mut buff = String::new();
@@ -62,7 +62,7 @@ fn main() {
       // Basicamente, quando um usuário digita algo do console nós
       // iremos escrever isso na string "buff" para que seja do tipo string
       // Isso tudo dentro de um loop para que possa ser enviadas multiplas mensagens
-      io::stdin().read_line(&mut buff).expect("Reading from stdin failed");
+      io::stdin().read_line(&mut buff).expect("\x1b[31mReading from stdin failed\x1b[0m");
 
       // Dando trim no buffer e jogando para dentro da variável msg que é do tipo string
       let msg = buff.trim().to_string();
@@ -72,5 +72,5 @@ fn main() {
       if msg == ":quit" || tx.send(msg).is_err() {break}
     }
     // Se alguém quebrar o loop essa mensagem será printada:
-    println!("Chat disconnected... bye!");
+    println!("\x1b[33mChat disconnected... bye!\x1b[0m");
 }
